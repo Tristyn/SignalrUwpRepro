898a804b8287a43a2bc10a8ca2c342671dc0cd91
Tristyn Stimpson <tristynstimpson@gmail.com>
2018-07-16 22:01:27 -0700

    Repro of 'Unable to connect to the server with any of the available transports.'

---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            
+            Init();
+
+            Debug.Assert(true);
+        }
+
+        private async void Init()
+        {
+            await Task.Delay(2000);
+            // Wait for server to listen
+
+            var hub = new HubConnectionBuilder()
+                .WithUrl("http://localhost:5000/api/v1/myhub")
+                .Build();
+
+            await hub.StartAsync();
+        }
+    }
+}
---
+++ WebApplication1/Hubs/MyHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Hubs
+{
+    public class MyHub : Hub<IMyHubClient>
+    {
+    }
+
+    public interface IMyHubClient
+    {
+
+    }
+}
